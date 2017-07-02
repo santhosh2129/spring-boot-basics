edd702f7df1235ffe057354d8c9259c83d27c69b
Santhosh <sthiyagarajan@owler.com>
2017-07-02 10:50:30 +0530

    completed ELK stack intergration

--- src/main/java/com/santhosh2129/server/service/impl/UserSingUpServiceImpl.java
+++ src/main/java/com/santhosh2129/server/service/impl/UserSingUpServiceImpl.java
@@ -6,6 +6,8 @@ import com.santhosh2129.common.persistence.repository.UserSignUpRepository;
 import com.santhosh2129.server.rest.entity.SigninRequestEntity;
 import com.santhosh2129.server.rest.entity.SignupRequestEntity;
 import com.santhosh2129.server.service.UserSingUpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ import java.util.Date;
  */
 @Service
 public class UserSingUpServiceImpl implements UserSingUpService {
+
+    private final static Logger log = LoggerFactory.getLogger( UserSingUpServiceImpl.class );
+
 
     @Autowired
     private UserSignUpRepository userSignUpRepository;
@@ -42,11 +47,14 @@ public class UserSingUpServiceImpl implements UserSingUpService {
     public String loginUser(SigninRequestEntity signinRequestEntity) {
         UserEntity userDocument = userSignUpRepository.findOne(signinRequestEntity.getUserName());
         if (userDocument == null) {
+            log.error("User not available");
             return "User Not Registered";
         }
         if (authenticationService.checkPassword(signinRequestEntity.getPassword(), userDocument.getPassword())) {
+            log.error("User available");
             return "Sign In Success";
         } else {
+            log.error("login failed");
             return "Wrong password";
         }
     }
